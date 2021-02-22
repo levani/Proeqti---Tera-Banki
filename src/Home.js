6516940b98ee38239a41decc66445323acef0cbd
Irena-Chadraniani <irenachadraniani@yahoo.com>
2021-02-22 22:59:55 +0400

    commit message

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import axios from 'axios';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams,
+    useHistory
+} from "react-router-dom"
 import './Home.css';
 import 'swiper/swiper-bundle.css';
 import slide1 from './slide1.png';
 import slide2 from './slide2.png';
+import sesxi1 from './sesxi1.png';
+import sesxi2 from './sesxi2.png';
+import barati1 from './barati1.png';
+import barati2 from './barati2.png';
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -20,14 +33,50 @@ SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 export default function Home() {
 
     return (
-        <Swiper class="swiper-container"
-      
-            slidesPerView={1}>
-            <SwiperSlide><img class="swiper-container" src={slide1} /></SwiperSlide>
-            <SwiperSlide><img class="swiper-container" src={slide2} /></SwiperSlide>
+        <div>
+            <Swiper class="swiper-container"
 
-            <div class="swiper-button-prev"></div>
-            <div class="swiper-button-next"></div>
-        </Swiper>
+                slidesPerView={1}>
+                <SwiperSlide><img class="swiper-container" src={slide1} /></SwiperSlide>
+                <SwiperSlide><img class="swiper-container" src={slide2} /></SwiperSlide>
+
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
+            </Swiper>
+
+            <div>
+                <div>
+                    <h2 className="title">იპოთეკური სესხები</h2>
+                    <Link className="title2">იხილეთ ყველა</Link>
+                </div>
+                
+                
+                <div className="sesxi">
+                <Link><img className="sesxi1" src={sesxi1} /></Link>
+                {/* <div className="title3"><h2>შეიძინე ბინა ისარგებლე სუბსიდიით</h2></div> */}
+                        </div>
+
+
+                <div>
+                    {/* <h2>იპოთეკური სესხი 12%-დან</h2> */}
+                    <Link><img className="sesxi2" src={sesxi2} /></Link>
+                </div>
+            </div>
+
+
+
+
+            <div>
+                <div><h2 className="title">პლასტიკური ბარათები</h2> </div>
+                <div> <Link className="title2">იხილეთ ყველა</Link> </div>
+                <div>
+                    <Link><img className="sesxi1" src={barati1} /></Link>
+                    <Link><img className="sesxi2" src={barati2} /></Link>
+                </div>
+            </div>
+
+        </div>
+
+
     )
 }
